87e0e6a92aa304557de4346d27322111d59a9925
seekseep <seekseep@gmail.com>
2021-12-02 15:37:05 +0900

    [DELETE] delete hooks

--- src/hooks/transaction.js
+++
@@ -1,9 +0,0 @@
-import { useMemo } from 'react'
-
-export function encodeMessage (message) {
-  return `0x${(new TextEncoder()).encode(message).map(v => v.toString(16))}`
-}
-
-export function useEncodedMessage (message) {
-  return useMemo(() => encodeMessage(message), [message])
-}
